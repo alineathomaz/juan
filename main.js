59ab812ff9675447297265b961458a41e5fa1f59
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-07-16 23:41:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+
+fabric.Image.fromURL ('mensagem.jpg', function (Img){
+
+blockImageObject = Img;
+
+blockImageObject.scaleToWidth(500);
+
+blockImageObject.scaleToHeight(450);
+
+blockImageObject.set({
+
+top:0,
+
+left:0
+
+});
+canvas.add(blockImageObject);
+});
+	
+}
+
+
+function playSound(){
+
+        x.play();
+        
+}
